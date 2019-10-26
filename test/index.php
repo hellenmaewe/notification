f32addc3b2bbce2aa7d9d613264a5fe9900b02ea
Hellem <hellen_maewe@hotmail.com>
2019-10-26 20:51:28 -0300

    teste fix

--- test/index.php
+++ test/index.php
@@ -6,8 +6,9 @@ use Notification\Email;
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception; // exceções
 
-$mail = new Email(2, "smtp.gmail.com", "hellen.maewep@gmail.com", "@maewe@1234", "tsl", '587', "hellen.maewep@gmail.com", "Hellen");
-$mail->sendMail();
+$email = new Email(2, "mail.host.com", "your@email.com", "your-pass", "smtp secure (tls / ssl)", "port (587)", "from@email.com", "From Name");
+
+$email->sendEmail "Subject", "Content", "reply@email.com", "Replay Name", "address@email.com", "Address Name");
 var_dump($mail);
 
 
